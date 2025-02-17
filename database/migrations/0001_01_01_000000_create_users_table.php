11621a0832f910a801fd69d8a338b352fef23d1b
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2025-02-17 20:47:33 -0300

    Update user entity to add profile_type field

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('profile_type', ['resident', 'admin', 'associate'])->default('resident');
             $table->rememberToken();
             $table->timestamps();
         });
