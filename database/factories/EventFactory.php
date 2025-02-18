b2b111d756d2e7a156c491af2b7c6cd471ecdcac
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2025-02-17 21:02:25 -0300

    Create Event entity

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'start_date' => $this->faker->date,
+            'end_date' => $this->faker->date,
+            'form_registration_url' => $this->faker->url,
+        ];
+    }
+}
