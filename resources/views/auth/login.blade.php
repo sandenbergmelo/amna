6d6797d5ae2bf3dad849f8ee9911abe26d56d89b
Osman Pereira <osmanpdo@gmail.com>
2025-02-24 07:10:30 -0300

    remove forgot password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,7 +34,6 @@
                         <div class="w-full py-2 flex justify-start flex-col">
                             <label for="password">Senha:</label>
                             <input class="w-full bg-white py-2 px-4 border border-black rounded" name="password" placeholder="Sua senha" type="password" minlength="8" required>
-                            <a class="py-1 text-blue-400 hover:text-blue-300 underline transition" href="">Esqueceu a senha?</a>
                         </div>
                         <div class="flex items-center space-x-2">
                             <label for="remember" class="text-gray-700 text-sm font-medium">Lembrar-se de mim</label>
@@ -64,7 +63,7 @@
                                 <span>{{ $message }}</span>
                             @enderror
                         </div>
-                        <span class="border border-gray-500 w-full mt-8"></span>
+                        <span class="border border-gray-500 w-full mt-5"></span>
                         <div class="bg-white w-full my-4 p-2">
                             <p>Ainda não tem uma conta?</p>
                             <a class="text-blue-400 hover:text-blue-300 underline transition" href="{{ route('register') }}">Se inscreva gratuitamente</a>
