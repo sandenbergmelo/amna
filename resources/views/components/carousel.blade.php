ec2fbad7a385498c459178c86a8aa17d2b39687c
Osman Pereira <osmanpdo@gmail.com>
2025-02-25 16:02:27 -0300

    Replace carousel images to new formats and remove old files

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,12 +1,12 @@
 <div class="relative max-w-4xl mx-auto p-4 border-2 border-transparent rounded-lg cursor-pointer w-[26rem] h-[13rem] lg:w-[50rem] lg:h-[25rem] md:w-[40rem] md:h-[20rem]" id="carousel">
     <div class="carousel-item active absolute top-0 left-0 w-full h-full opacity-0 transition-opacity duration-1000 ease-in-out z-1">
-        <img src="{{ asset('carousel/carrousel-1.jpg') }}" class="w-full h-full object-cover rounded-lg shadow-lg">
+        <img src="{{ asset('carousel/carousel-1.jpeg') }}" class="w-full h-full object-cover rounded-lg shadow-lg">
     </div>
     <div class="carousel-item absolute top-0 left-0 w-full h-full opacity-0 transition-opacity duration-1000 ease-in-out z-1">
-        <img src="{{ asset('carousel/carrousel-2.jpg') }}" class="w-full h-full object-cover rounded-lg shadow-lg">
+        <img src="{{ asset('carousel/carousel-2.jpeg') }}" class="w-full h-full object-cover rounded-lg shadow-lg">
     </div>
     <div class="carousel-item absolute top-0 left-0 w-full h-full opacity-0 transition-opacity duration-1000 ease-in-out z-1">
-        <img src="{{ asset('carousel/carrousel-3.jpg') }}" class="w-full h-full object-cover rounded-lg shadow-lg">
+        <img src="{{ asset('carousel/carousel-3.jpeg') }}" class="w-full h-full object-cover rounded-lg shadow-lg">
     </div>
     <button class="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full z-10" onclick="prevSlide()">&#10094;</button>
     <button class="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full z-10" onclick="nextSlide()">&#10095;</button>
